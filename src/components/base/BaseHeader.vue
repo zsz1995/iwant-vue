1f6b5d58f8f4e5d367eb432470c2ff3444ca5cd6
咩咩 <oncwnuLCyaO7t7aE98I_-mBLkLiw@git.weixin.qq.com>
2019-12-25 17:39:48 +0800

    注册抽屉

--- src/components/base/BaseHeader.vue
+++ src/components/base/BaseHeader.vue
@@ -15,17 +15,32 @@
         <el-button-group>
           <el-button @click="login_drawer = true" type="text" icon="icon-login">登录</el-button>
           <el-button type="text">/</el-button>
-          <el-button type="text" icon="icon-adduser">注册</el-button>
+          <el-button @click="register_drawer = true" type="text" icon="icon-adduser">注册</el-button>
         </el-button-group>
       </span>
       </el-menu>
     </el-header>
     <el-drawer
-      title="登录"
-      :visible.sync="login_drawer"
-      :before-close="loginCloseHandler"
+    title="登录"
+    :visible.sync="login_drawer"
     >
+    </el-drawer>
 
+    <el-drawer
+    title="注册"
+    :visible.sync="register_drawer"
+    >
+      <el-form :model="input" :rules="rules" ref="ruleForm" class="iw-drawform">
+        <el-form-item label="手机号" required prop="tel">
+          <el-input v-model="input.tel" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="pass">
+          <el-input v-model="input.pass" type="password" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="确认密码" prop="checkPass">
+          <el-input v-model="input.checkPass" type="password" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
     </el-drawer>
   </div>
 </template>
@@ -37,8 +52,30 @@
       activeIndex: String
     },
     data() {
+      var validatePass = (rule, value, callback) => {
+        if (value === "") {
+          callback(new Error("请输入密码"))
+        }else {
+          if (this.input.pass !== "") {
+            this.$refs.ruleForm.validateField("checkPass");
+          }
+        }
+      };
+
+
       return {
-        login_drawer: false
+        input: {
+          tel: "",
+          pass: "",
+          checkPass: ""
+        },
+        rules: {
+          pass:[
+            {validator: validatePass, trigger: "blur"}
+          ]
+        },
+        login_drawer: false,
+        register_drawer: true
       }
     },
     methods: {
@@ -46,7 +83,8 @@
         this.$confirm("确认关闭?").then(() => {
           done();
         })
-        .catch(() => {})
+        .catch(() => {
+        })
       }
     }
   }
@@ -62,4 +100,7 @@
     min-width: 100%;
   }
 
+  .iw-drawform {
+    margin: 20px 30px;
+  }
 </style>
